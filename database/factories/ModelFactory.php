d32d5e089fd9b2b9fcc0e65c0a71b5432888fa9f
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-09-06 17:57:31 +0300

    #44 notification test refactoring

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -57,3 +57,14 @@ $factory->define(App\Reply::class, function (Faker\Generator $faker) {
 });
 
 
+$factory->define(\Illuminate\Notifications\DatabaseNotification::class, function (Faker\Generator $faker) {
+    return [
+        'id' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
+        'type' => 'App\Notifications\ThreadWasUpdated',
+        'notifiable_id' => function() {
+            return auth()->id() ?: factory('App\User')->create()->id;
+        },
+        'notifiable_type' => 'App\User',
+        'data' => [ 'foo' => 'bar'],
+    ];
+});
--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Notifications\DatabaseNotification;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -9,11 +10,16 @@ class NotificationsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->signIn();
+    }
+
     /** @test */
     public function a_notification_is_prepaired_when_a_subscribed_thread_receives_a_new_reply_by_other_user()
     {
         // Given we have a user and a thread and the user is subscibed to the thread
-        $this->signIn();
         $thread = create('App\Thread')->subscribe();
         $this->assertCount(0, auth()->user()->notifications);        
         // When new reply is posted to the thread by this user
@@ -21,9 +27,9 @@ class NotificationsTest extends TestCase
             'user_id' => auth()->id(),
             'body' =>  'Some reply body here.'
         ]);
-        // Then no notification created
+        // Then no notification is created
         $this->assertCount(0, auth()->user()->fresh()->notifications);
-        // When new reply is posted to the thread by another user
+        // When new reply is posted to the thread by a different user
         $thread->addReply([ 
             'user_id' => create('App\User')->id,
             'body' =>  'Some reply body here.'
@@ -35,18 +41,11 @@ class NotificationsTest extends TestCase
     /** @test */
     public function a_user_can_fetch_its_unread_notifications()
     {
-        // Given we have a user and a thread and the user is subscibed to the thread
-        $this->signIn();
-        $thread = create('App\Thread')->subscribe();
-        // .. and user gets notification if new repy is posted
-        $thread->addReply([ 
-            'user_id' => create('App\User')->id,
-            'body' =>  'Some reply body here.'
-        ]);
+        // Given we have a authenticated user and notification
+        create(DatabaseNotification::class);
         $user = auth()->user();
-
         // When the user hits gets notifications route
-        $response = $this->getJson("/profiles/{$user->name}/notifications")->json();
+        $response = $this->getJson('/profiles/' . auth()->user()->name . '/notifications')->json();
         // Then the user gets his collection of unread notification
         $this->assertCount(1, $response);
     }
@@ -54,21 +53,17 @@ class NotificationsTest extends TestCase
     /** @test */
     public function a_user_can_mark_a_notification_as_read()
     {
-        // Given we have a user and a thread and the user is subscibed to the thread
-        $this->signIn();
-        $thread = create('App\Thread')->subscribe();
-        // .. and user gets notification if new repy is posted
-        $thread->addReply([ 
-            'user_id' => create('App\User')->id,
-            'body' =>  'Some reply body here.'
-        ]);
-        $user = auth()->user();
-        $this->assertCount(1, $user->unreadNotifications);
+        // Given we have a authenticated user and notification
+        create(DatabaseNotification::class);
+        tap($user = auth()->user(), function ($user) {
+            // Check that we have a notification 
+            $this->assertCount(1, $user->unreadNotifications);
+            // When user clears the notification
+            $notificationId = $user->notifications->first()->id;
+            $this->delete("/profiles/{$user->name}/notifications/{$notificationId}");
+            // Then the user notifications are cleared
+            $this->assertCount(0, $user->fresh()->unreadNotifications);
+        });
 
-        // When user clears the notification
-        $notificationId = $user->notifications->first()->id;
-        $this->delete("/profiles/{$user->name}/notifications/{$notificationId}");
-        // Then the user notifications are cleared
-        $this->assertCount(0, $user->fresh()->unreadNotifications);
     }
 }
